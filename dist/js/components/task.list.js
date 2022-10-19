1588a88a313b75528b195ae7db95235d561ade28
Liluminalia <peloazul4@gmail.com>
2022-10-19 10:59:01 +0200

    adding all files

---
+++ dist/js/components/task.list.js
@@ -0,0 +1,50 @@
+import { TASKS } from '../models/data.js';
+import { Task } from '../models/task.js';
+import { AddTask } from './add.task.js';
+import { Component } from './component.js';
+export class TaskList extends Component {
+    constructor(selector) {
+        super();
+        this.selector = selector;
+        this.tasks = [...TASKS];
+        this.manageComponent();
+    }
+    manageComponent() {
+        this.template = this.createTemplate();
+        this.render(this.selector, this.template);
+        new AddTask('slot#add-task', this.handleAdd.bind(this));
+        setTimeout(() => {
+            document
+                .querySelectorAll('.eraser')
+                .forEach((item) => item.addEventListener('click', this.handlerEraser.bind(this)));
+        }, 100);
+    }
+    createTemplate() {
+        let template = `<section>
+                <h2>Tareas</h2>
+                <slot id="add-task"></slot>
+                <ul>`;
+        this.tasks.forEach((item) => {
+            template += `
+            <li> ${item.id} - ${item.title} 
+            <span class="eraser" data-id="${item.id}">🗑️</span>
+            </li>`;
+        });
+        template += `</ul>
+            </section>`;
+        return template;
+    }
+    handleAdd(ev) {
+        ev.preventDefault();
+        const title = document.querySelector('#title')
+            .value;
+        const responsible = document.querySelector('#resp').value;
+        this.tasks.push(new Task(title, responsible));
+        this.manageComponent();
+    }
+    handlerEraser(ev) {
+        const deletedID = ev.target.dataset.id;
+        this.tasks = this.tasks.filter((item) => item.id !== +deletedID);
+        this.manageComponent();
+    }
+}
